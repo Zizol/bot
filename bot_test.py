2d831dacae7520faf21a2f877254fa84c7285433
Zizol <adrien.videgrain@gmail.com>
2018-10-25 17:46:06 +0200

    Commande non reconnue

--- bot_test.py
+++ bot_test.py
@@ -42,16 +42,22 @@ def inline_caps(bot, update):
     bot.answer_inline_query(update.inline_query.id, results)
 
 
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+
+
 #Add handlers (/start, message) to the dispatcher. Allows passing arguments for /caps
 start_handler = CommandHandler('start', start)
 echo_handler = MessageHandler(Filters.text, echo)
 caps_handler = CommandHandler('caps', caps, pass_args=True)
 inline_caps_handler = InlineQueryHandler(inline_caps)
+unknown_handler = MessageHandler(Filters.command, unknown)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(caps_handler)
 dispatcher.add_handler(inline_caps_handler)
+dispatcher.add_handler(unknown_handler)
 
 #Activate bot
 updater.start_polling()
