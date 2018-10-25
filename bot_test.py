1d533672acd6d909ac2fcf5d89622e573c4200df
Zizol <adrien.videgrain@gmail.com>
2018-10-25 17:19:42 +0200

    Merge branch 'master' of https://github.com/Zizol/bot

--- bot_test.py
+++ bot_test.py
@@ -1,7 +1,25 @@
-import telegram
+from telegram.ext import Updater, CommandHandler
+import logging
 
+#Log issues
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+#Authentication token obtained through botfather
 Token = "749231366:AAFs4JxqBW-OBZGcf8BaqjwMtQIHdsHRQkM"
-bot = telegram.Bot(token=Token)
 
-print(bot.get_me())
-print("ptdr")
+#Initialize and link updater (input) and dispatcher (output)
+updater = Updater(token=Token)
+dispatcher = updater.dispatcher
+
+#Function to activate on sending a message to the bot
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+#Add handler (command /start) to the dispatcher
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+#Activate bot
+updater.start_polling()
