03ca703d1b7485083cd5849487ad3c97916a53c5
Zizol <adrien.videgrain@gmail.com>
2018-10-25 17:39:50 +0200

    Inline

--- bot_test.py
+++ bot_test.py
@@ -1,4 +1,6 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,
+                        InlineQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
 import logging
 
 #Log issues
@@ -26,14 +28,31 @@ def caps(bot, update, args):
     bot.send_message(chat_id=update.message.chat_id, text=text_caps)
 
 
+def inline_caps(bot, update):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    bot.answer_inline_query(update.inline_query.id, results)
+    
+
 #Add handlers (/start, message) to the dispatcher. Allows passing arguments for /caps
 start_handler = CommandHandler('start', start)
 echo_handler = MessageHandler(Filters.text, echo)
 caps_handler = CommandHandler('caps', caps, pass_args=True)
+inline_caps_handler = InlineQueryHandler(inline_caps)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(caps_handler)
+dispatcher.add_handler(inline_caps_handler)
 
 #Activate bot
 updater.start_polling()
