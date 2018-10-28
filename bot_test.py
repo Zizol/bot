f9be167152cc0d279a5dc92ccc7e3bbd0ccf5d79
Zizol <adrien.videgrain@gmail.com>
2018-10-28 19:48:34 +0100

    Multiple changes

--- bot_test.py
+++ bot_test.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 import logging
@@ -19,7 +22,7 @@ def start(bot, update):
 
 
 def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.from_user.first_name)
 
 
 def caps(bot, update, args):
@@ -27,36 +30,47 @@ def caps(bot, update, args):
     bot.send_message(chat_id=update.message.chat_id, text=text_caps)
 
 
-def inline_caps(bot, update):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    bot.answer_inline_query(update.inline_query.id, results)
-
-
 def unknown(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+
+from telegram.error import (TelegramError, Unauthorized, BadRequest,
+                            TimedOut, ChatMigrated, NetworkError)
+
+# def error_callback(bot, update, error):
+#     try:
+#         raise error
+#     except Unauthorized:
+#         print("allo")
+#         # remove update.message.chat_id from conversation list
+#     except BadRequest:
+#         print("allo")
+#         # handle malformed requests - read more below!
+#     except TimedOut:
+#         print("allo")
+#         # handle slow connection problems
+#     except NetworkError:
+#         print("allo")
+#         # handle other connection problems
+#     except ChatMigrated as e:
+#         print("allo")
+#         # the chat_id of a group has changed, use e.new_chat_id instead
+#     except TelegramError:
+#         print("allo")
+#         # handle all other telegram related errors
+#
+# dispatcher.add_error_handler(error_callback)
+
 
 
 #Add handlers (/start, message) to the dispatcher. Allows passing arguments for /caps
 start_handler = CommandHandler('start', start)
 echo_handler = MessageHandler(Filters.text, echo)
 caps_handler = CommandHandler('caps', caps, pass_args=True)
-inline_caps_handler = InlineQueryHandler(inline_caps)
 unknown_handler = MessageHandler(Filters.command, unknown)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(caps_handler)
-dispatcher.add_handler(inline_caps_handler)
 dispatcher.add_handler(unknown_handler)
 
 #Activate bot
